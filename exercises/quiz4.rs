fa7756772355feaed54dff2e5381db50f4986e21
Scott Nicholes <scott93gps@gmail.com>
2023-04-26 00:14:37 -0700

    Completed Quiz 4

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,7 +5,15 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+#[macro_use]
+mod macros {
+    macro_rules! my_macro {
+        ($x:expr) => {
+            format!("Hello {}", $x)
+        };
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
