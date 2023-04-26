13bc47e66931a07ab32d9818abd9f48787064b4e
Scott Nicholes <scott93gps@gmail.com>
2023-04-26 00:01:14 -0700

    Completed Macros exercises

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -2,16 +2,22 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` for hints :)
 
-// I AM NOT DONE
-
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
+
+    macro_rules! greet {
+        ($x:expr) => {
+            println!("Hello {}!", $x);
+        };
+    }
 }
 
 fn main() {
     my_macro!();
+    greet!("Scott");
 }
