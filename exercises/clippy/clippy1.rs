d3b5cb8ed5bd7651e6730aa06be6212ba4852f91
Scott Nicholes <scott93gps@gmail.com>
2023-04-26 02:45:35 -0700

    Completed clippy and first 2 conversions exercises.

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,12 +6,13 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
+    // We need to add this error margin because comparing f64 values is not
+    //  recommended by Clippy
+    let error_margin = f64::EPSILON;
     let x = 1.2331f64;
     let y = 1.2332f64;
-    if y != x {
+    if (y - x).abs() > error_margin {
         println!("Success!");
     }
 }
